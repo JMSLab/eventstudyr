5b8f7bf3715de9ce72146a3f556f345a73a54138
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2022-01-20 09:48:55 -0500

    #2 Add estimatr and fixest benchmarks

--- issue2/compare_fe_packages.R
+++ issue2/compare_fe_packages.R
@@ -4,6 +4,7 @@ pacman::p_load(tidyverse,
                estimatr,
                fixest,
                lmtest,
+               rbenchmark,
                kableExtra)
 
 PATH_TO_DATA <- "C:/Users/c1nhs01/Downloads/Burda-Harding.csv"
@@ -40,16 +41,12 @@ RunFixest <- function(se_method) {
   
   reg_table <- reg %>%
     tidy() %>% 
-    transmute(
+    mutate(
       package = "fixest",
-      term,
       se_type = se_method,
-      estimate,
-      std.error,
-      statistic,
-      p.value,
-      df
-    )
+      df = df
+    ) %>% 
+    select(package, term, se_type, estimate, std.error, statistic, p.value, df)
   
   return(reg_table)
   
@@ -78,14 +75,52 @@ RunPlm <- function(se_method) {
   
 }
 
+estimatr_stata <- benchmark(estimatr::lm_robust(fe_formula, data = df_raw, fixed_effects = ~ FIRM + YEAR, se_type = "stata"), replications = 1000)
+estimatr_HC0 <- benchmark(estimatr::lm_robust(fe_formula, data = df_raw, fixed_effects = ~ FIRM + YEAR, se_type = "HC0"), replications = 1000)
+
+fixest_HC1<- benchmark(fixest::feols(fe_formula, data = df_raw, fixef = c("FIRM", "YEAR"), vcov = "HC1"), replications = 1000)
+fixest_twoway <- benchmark(fixest::feols(fe_formula, data = df_raw, fixef = c("FIRM", "YEAR"), vcov = "twoway"), replications = 1000)
+
+CalcAvgRuntime <- function(benchmark_data) {
+  
+  num_replications <- benchmark_data$replications
+  elapsed_time <- benchmark_data$elapsed
+  
+  return(elapsed_time / num_replications)
+  
+}
+
+
+`avg_run_time (s)` <- c(CalcAvgRuntime(estimatr_stata), 
+                        CalcAvgRuntime(fixest_HC1), 
+                        CalcAvgRuntime(estimatr_HC0), 
+                        CalcAvgRuntime(fixest_twoway)) %>% 
+  rep(., each = 3)
+
 df_results <- bind_rows(
   RunEstimatr("stata"),
   RunFixest("HC1"),
   RunEstimatr("HC0"),
-  RunFixest("twoway"),
-  RunPlm("HC0"),
-  RunPlm("HC1")
-)
+  RunFixest("twoway")
+) %>% 
+  mutate(
+    `avg_run_time (s)`
+  )
 
 kable(df_results, digits = 4, format = "simple", row.names = TRUE) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

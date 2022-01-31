0ecb70dcdf33f439a9cff5a0519067e165dbc39e
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2022-01-31 16:35:32 -0500

    #2 Move data prep to EventStudy()

--- R/PrepareModelFormula.R
+++ R/PrepareModelFormula.R
@@ -34,52 +34,24 @@
 #' \dontrun{
 #' PrepareModelFormula("OLS", gapminder::gapminder, outcomevar = "lifeExp",
 #' policyvar = "pop", idvar = "continent", timevar = "year",
-#' controls = "gdpPercap", M = 3, G = 2, LG = 3, LM = 4)
+#' controls = "gdpPercap", M = 3, G = 2, LG = 5, LM = 4)
 #'
 #' }
 
 
-PrepareModelFormula <- function(estimator, data, outcomevar, policyvar, idvar, timevar, controls = NULL,
-                                proxy = NULL, proxyIV = NULL, M, LM = 1, G, LG = M + G,
-                                normalize = -1) {
+PrepareModelFormula <- function(policyvar, controls, str_policy_fd, str_policy_lead, str_policy_lag, outcomevar) {
 
-    if (! estimator %in% c("OLS", "FHS")) {stop("estimator should be either 'OLS' or 'FHS'.")}
-    if (! is.data.frame(data)) {stop("data should be a data frame.")}
-    if (! is.character(outcomevar)) {stop("outcomevar should be a character.")}
-    if (! is.character(policyvar)) {stop("policyvar should be a character.")}
-    if (! is.character(idvar)) {stop("idvar should be a character.")}
-    if (! is.character(timevar)) {stop("timevar should be a character.")}
-    if (! (is.null(controls) | is.character(controls))) {stop("controls should be either NULL or a character.")}
-    if ((estimator == "OLS" & ! is.null(proxy))) {stop("proxy should only be specified when estimator = 'FHS'.")}
-    if ((estimator == "FHS" & ! is.character(proxy))) {stop("proxy should be a character.")}
-    if ((estimator == "OLS" & ! is.null(proxyIV))) {stop("proxyIV should only be specified when estimator = 'FHS'.")}
-    if ((estimator == "FHS" & ! is.character(proxyIV))) {stop("proxyIV should be a character.")}
-    if (! (is.numeric(M) & M > 0)) {stop("M should be a positive integer.")}
-    # should LM be > 0?
-    if (! (is.numeric(LM) & LM > 0)) {stop("LM should be a positive integer.")}
-    if (! (is.numeric(G) & G > 0)) {stop("G should be a positive integer.")}
-    # should LG be > 0?
-    if (! (is.numeric(LG) & LG > 0)) {stop("LG should be a positive integer.")}
-    # should normalize be a negative integer?
-    if (!is.numeric(normalize)) {stop("normalize should be numeric.")}
-
-    df_policy_leads <- PrepareLeads(data, groupvar = idvar, timevar = timevar, leadvar = policyvar, leads = 1:M)
-    df_policy_lags <- PrepareLags(data, groupvar = idvar, timevar = timevar, lagvar = policyvar, lags = 1:G)
-
-    str_policy_leads <- names(dplyr::select(df_policy_leads, dplyr::matches("_lead[1-9]+[0-9]*$")))
-    str_policy_lags <- names(dplyr::select(df_policy_lags, dplyr::matches("_lag[1-9]+[0-9]*$")))
-
-    reg_formula <- reformulate(termlabels = c(policyvar, controls, str_policy_leads, str_policy_lags),
+    reg_formula <- reformulate(termlabels = c(policyvar, controls, str_policy_fd, str_policy_lead, str_policy_lag),
                                response = outcomevar)
 
-    df_leads_lags <- cbind(data, df_policy_leads, df_policy_lags)
+    return(reg_formula)
 
-    as.formula(
-        estimatr::lm_robust(
-        formula = reg_formula,
-        data = df_leads_lags,
-        fixed_effects = get(idvar) ~ get(timevar)
-        )
-    )
+    # as.formula(
+    #     estimatr::lm_robust(
+    #     formula = reg_formula,
+    #     data = df_leads_lags,
+    #     fixed_effects = get(idvar) ~ get(timevar)
+    #     )
+
 
 }

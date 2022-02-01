21a5317b05d6c73ea5c1fcb93ca06c194aee4642
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2022-02-01 11:12:43 -0500

    #2 Update PrepareModelFormula documentation

--- R/PrepareModelFormula.R
+++ R/PrepareModelFormula.R
@@ -1,57 +1,33 @@
 
 #' Prepares a formula object for use in EventStudyOLS or EventStudyFHS
 #'
-#' @param estimator Accepts one of "OLS" or "FHS". If "FHS" is specified, implements IV.
-#' @param data The data frame that contains the variables of interest.
+
 #' @param outcomevar Variable indicating outcomme variable y, should be a character.
 #' @param policyvar Variable indicating policy variable z, should be a character.
-#' @param idvar Variable indicating units, should be a character.
-#' @param timevar Variable indicating time periods, should be a character.
+#' @param str_policy_fd Vector indicating leads and lags of first differenced policy variable z with the (G +1)th term omitted for normalization, should be a string.
+#' @param str_policy_lead Variable indicating the (G + LG)th lead of the policy variable z, should be a character.
+#' @param str_policy_lag Variable indicating the (M + LM)th lag of the policy variable z, should be a character.
 #' @param controls Optional vector of controls q, should be a character.
-#' @param proxy Variable that is thought to be affected by the confoud but not by the policy.
-#' Should be specified if and only if estimator is specified as "FHS". Should be a character.
-#' @param proxyIV Variables to be used as an instrument. For the case of a single proxy,
-#' defaults to the strongest lead of the policy variable based on the first stage.
-#' Should be specified if an only if estimator is specified as "FHS".
-#' Should be a character
-#' @param M The number of periods in the past before which the past values of the policy
-#' are not supposed to affect the value of the outcome. Should be a positive integer.
-#' @param LM Optional number of event times after M to be included in estimation. Defaults to 1.
-#' Should be a positive integer.
-#' @param G Number of periods in the future after which the future values of the policy are
-#' not supposed to affect the value of the outcome today. Should be a positive integer.
-#' @param LG Optional number of event times earlier than -G to be included in estimation. Defaults to M + G.
-#' Should be a positive integer.
-#' @param normalize Specifies the event-time coefficient to be normalized. Defaults to -1.
-#' Should be an integer. Should be one of TRUE or FALSE.
+
 #'
 #' @return A formula object to be passed to EventStudy
-#' @import estimatr
-#' @import dplyr
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#' PrepareModelFormula("OLS", gapminder::gapminder, outcomevar = "lifeExp",
-#' policyvar = "pop", idvar = "continent", timevar = "year",
-#' controls = "gdpPercap", M = 3, G = 2, LG = 5, LM = 4)
-#'
-#' }
+#' PrepareModelFormula(outcomevar = "lifeExp",
+#' policyvar = "pop",
+#' str_policy_fd = c("pop_fd_lead1", "pop_fd_lead2", "pop_fd_lead4",
+#' "pop_fd_lead5", "pop_fd_lead6", "pop_fd_lag1", "pop_fd_lag2",
+#' "pop_fd_lag3", "pop_fd_lag4", "pop_fd_lag5", "pop_fd_lag6", "pop_fd_lag7"),
+#' str_policy_lead = "pop_lead6",
+#' str_policy_lag = "pop_lag8",
+#' controls = "gdpPercap")
 
-
-PrepareModelFormula <- function(policyvar, controls, str_policy_fd, str_policy_lead, str_policy_lag, outcomevar) {
+PrepareModelFormula <- function(outcomevar, policyvar, str_policy_fd, str_policy_lead, str_policy_lag, controls) {
 
     reg_formula <- reformulate(termlabels = c(policyvar, controls, str_policy_fd, str_policy_lead, str_policy_lag),
                                response = outcomevar)
 
     return(reg_formula)
-
-    # as.formula(
-    #     estimatr::lm_robust(
-    #     formula = reg_formula,
-    #     data = df_leads_lags,
-    #     fixed_effects = get(idvar) ~ get(timevar)
-    #     )
-
 
 }

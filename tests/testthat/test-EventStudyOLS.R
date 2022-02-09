44db6aa8e75f1c679bfe1f6e82ee1e03df543f39
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2022-02-09 10:35:27 -0500

    #2 Test agreement between base STATA and EventStudyOLS

--- tests/testthat/test-EventStudyOLS.R
+++ tests/testthat/test-EventStudyOLS.R
@@ -5,7 +5,7 @@ test_that("FE = TRUE,
 
     df_test_EventStudyOLS <- read.csv("./input/df_test_EventStudyOLS.csv")
 
-    outcomevar      <- "z"
+    outcomevar      <- "y_base"
     str_policy_fd   <- c("z_fd", "z_fd_lead2", "z_fd_lead3", "z_fd_lag1", "z_fd_lag2")
     str_policy_lead <-"z_lead3"
     str_policy_lag  <- "z_lag3"
@@ -46,7 +46,7 @@ test_that("FE = FALSE,
 
     df_test_EventStudyOLS <- read.csv("./input/df_test_EventStudyOLS.csv")
 
-    outcomevar      <- "z"
+    outcomevar      <- "y_base"
     str_policy_fd   <- c("z_fd", "z_fd_lead2", "z_fd_lead3", "z_fd_lag1", "z_fd_lag2")
     str_policy_lead <-"z_lead3"
     str_policy_lag  <- "z_lag3"
@@ -87,7 +87,7 @@ test_that("FE = TRUE,
 
     df_test_EventStudyOLS <- read.csv("./input/df_test_EventStudyOLS.csv")
 
-    outcomevar      <- "z"
+    outcomevar      <- "y_base"
     str_policy_fd   <- c("z_fd", "z_fd_lead2", "z_fd_lead3", "z_fd_lag1", "z_fd_lag2")
     str_policy_lead <-"z_lead3"
     str_policy_lag  <- "z_lag3"
@@ -128,7 +128,7 @@ test_that("FE = FALSE,
 
     df_test_EventStudyOLS <- read.csv("./input/df_test_EventStudyOLS.csv")
 
-    outcomevar      <- "z"
+    outcomevar      <- "y_base"
     str_policy_fd   <- c("z_fd", "z_fd_lead2", "z_fd_lead3", "z_fd_lag1", "z_fd_lag2")
     str_policy_lead <-"z_lead3"
     str_policy_lag  <- "z_lag3"
@@ -169,7 +169,7 @@ test_that("FE = TRUE,
 
     df_test_EventStudyOLS <- read.csv("./input/df_test_EventStudyOLS.csv")
 
-    outcomevar      <- "z"
+    outcomevar      <- "y_base"
     str_policy_fd   <- c("z_fd", "z_fd_lead2", "z_fd_lead3", "z_fd_lag1", "z_fd_lag2")
     str_policy_lead <-"z_lead3"
     str_policy_lag  <- "z_lag3"
@@ -210,7 +210,7 @@ test_that("FE = FALSE,
 
     df_test_EventStudyOLS <- read.csv("./input/df_test_EventStudyOLS.csv")
 
-    outcomevar      <- "z"
+    outcomevar      <- "y_base"
     str_policy_fd   <- c("z_fd", "z_fd_lead2", "z_fd_lead3", "z_fd_lag1", "z_fd_lag2")
     str_policy_lead <-"z_lead3"
     str_policy_lag  <- "z_lag3"
@@ -251,7 +251,7 @@ test_that("FE = TRUE,
 
     df_test_EventStudyOLS <- read.csv("./input/df_test_EventStudyOLS.csv")
 
-    outcomevar      <- "z"
+    outcomevar      <- "y_base"
     str_policy_fd   <- c("z_fd", "z_fd_lead2", "z_fd_lead3", "z_fd_lag1", "z_fd_lag2")
     str_policy_lead <-"z_lead3"
     str_policy_lag  <- "z_lag3"
@@ -292,7 +292,7 @@ test_that("FE = FALSE,
 
     df_test_EventStudyOLS <- read.csv("./input/df_test_EventStudyOLS.csv")
 
-    outcomevar      <- "z"
+    outcomevar      <- "y_base"
     str_policy_fd   <- c("z_fd", "z_fd_lead2", "z_fd_lead3", "z_fd_lag1", "z_fd_lag2")
     str_policy_lead <-"z_lead3"
     str_policy_lag  <- "z_lag3"
@@ -324,5 +324,62 @@ test_that("FE = FALSE,
     expect_true(is.null(reg$felevels$`get(timevar)`), TRUE)
     expect_true(is.null(reg$nclusters), TRUE)
     expect_true(reg$se_type %in% c("stata", "HC1"))
+
+})
+
+
+test_that("Coefficients and Standard Errors agree with base STATA", {
+
+    df_test_EventStudyOLS <- read.csv("./input/df_test_EventStudyOLS.csv")
+
+    outcomevar      <- "y_base"
+    str_policy_fd   <- c("z_fd", "z_fd_lead2", "z_fd_lead3", "z_fd_lag1", "z_fd_lag2")
+    str_policy_lead <-"z_lead3"
+    str_policy_lag  <- "z_lag3"
+    controls        <- "x_r"
+
+    event_study_formula <- PrepareModelFormula(outcomevar, str_policy_fd, str_policy_lead, str_policy_lag, controls)
+
+    idvar   <- "id"
+    timevar <- "t"
+
+    FE      <- TRUE
+    TFE     <- TRUE
+    cluster <- TRUE
+
+    reg <- EventStudyOLS(
+            prepared_model_formula = event_study_formula,
+            prepared_data = df_test_EventStudyOLS,
+            idvar = idvar,
+            timevar = timevar,
+            FE = FE,
+            TFE = TFE,
+            cluster = cluster
+        )
+
+    df_test_STATA <- read.csv("./input/df_test_base_STATA_1_1_2_2.csv")
+
+    epsilon <- 10e-7
+
+    expect_equal(unname(reg$coefficients["z_fd"]), as.double(df_test_STATA[df_test_STATA["term"] == "zfd"][2]), tolerance = epsilon)
+    expect_equal(unname(reg$coefficients["z_fd_lead2"]), as.double(df_test_STATA[df_test_STATA["term"] == "F2"][2]), tolerance = epsilon)
+    expect_equal(unname(reg$coefficients["z_fd_lead3"]), as.double(df_test_STATA[df_test_STATA["term"] == "F3"][3]), tolerance = epsilon)
+    expect_equal(unname(reg$coefficients["z_fd_lag1"]), as.double(df_test_STATA[df_test_STATA["term"] == "L1"][2]), tolerance = epsilon)
+    expect_equal(unname(reg$coefficients["z_fd_lag2"]), as.double(df_test_STATA[df_test_STATA["term"] == "L2"][2]), tolerance = epsilon)
+    expect_equal(unname(reg$coefficients["z_lead3"]), as.double(df_test_STATA[df_test_STATA["term"] == "F3"][4]), tolerance = epsilon)
+    expect_equal(unname(reg$coefficients["z_lag3"]), as.double(df_test_STATA[df_test_STATA["term"] == "L3"][2]), tolerance = epsilon)
+    expect_equal(unname(reg$coefficients["x_r"]), as.double(df_test_STATA[df_test_STATA["term"] == "x_r"][2]), tolerance = epsilon)
+
+    expect_equal(unname(reg$std.error["z_fd"]), as.double(df_test_STATA[df_test_STATA["term"] == "zfd"][3]), tolerance = epsilon)
+    expect_equal(unname(reg$std.error["z_fd_lead2"]), as.double(df_test_STATA[df_test_STATA["term"] == "F2"][3]), tolerance = epsilon)
+    expect_equal(unname(reg$std.error["z_fd_lead3"]), as.double(df_test_STATA[df_test_STATA["term"] == "F3"][5]), tolerance = epsilon)
+    expect_equal(unname(reg$std.error["z_fd_lag1"]), as.double(df_test_STATA[df_test_STATA["term"] == "L1"][3]), tolerance = epsilon)
+    expect_equal(unname(reg$std.error["z_fd_lag2"]), as.double(df_test_STATA[df_test_STATA["term"] == "L2"][3]), tolerance = epsilon)
+    expect_equal(unname(reg$std.error["z_lead3"]), as.double(df_test_STATA[df_test_STATA["term"] == "F3"][6]), tolerance = epsilon)
+    expect_equal(unname(reg$std.error["z_lag3"]), as.double(df_test_STATA[df_test_STATA["term"] == "L3"][3]), tolerance = epsilon)
+    expect_equal(unname(reg$std.error["x_r"]), as.double(df_test_STATA[df_test_STATA["term"] == "x_r"][3]), tolerance = epsilon * 10)
+
+
+
 
 })

1cba4a7f3741a7ca41edb7ffff3bc6198c06f45d
Veli Murat ANDIRIN <62054739+veli-m-andirin@users.noreply.github.com>
2022-03-02 09:08:51 -0500

    #2 reintroduces a test that was removed by mistake

--- tests/testthat/test-EventStudy.R
+++ tests/testthat/test-EventStudy.R
@@ -30,32 +30,6 @@ test_that("correctly throws an error when normalized coefficient is outside even
     post  <- 2
     pre  <- 3
     overidpre <- 4
-    overidpost <- 11
-
-    outputs <- EventStudy(estimator = "OLS", data = df_sample_dynamic, outcomevar = "y_base",
-                          policyvar = "z", idvar = "id", timevar = "t",
-                          controls = "x_r", FE = TRUE, TFE = TRUE,
-                          post = post, pre = pre, overidpre = overidpre, overidpost = overidpost, normalize = - 1, cluster = TRUE)
-
-    leads_lags      <- outputs[[1]]$term
-    largest_fd_lag  <- as.double(stringr::str_extract(leads_lags, "(?<=fd_lag)[0-9]+"))
-    largest_fd_lead <- as.double(stringr::str_extract(leads_lags, "(?<=fd_lead)[0-9]+"))
-    largest_lag     <- as.double(stringr::str_extract(leads_lags, "(?<=lag)[0-9]+"))
-    largest_lead    <- as.double(stringr::str_extract(leads_lags, "(?<=lead)[0-9]+"))
-
-    expect_equal(max(largest_fd_lag, na.rm = TRUE), post + overidpost - 1)
-    expect_equal(max(largest_fd_lead, na.rm = TRUE), pre + overidpre)
-    expect_equal(max(largest_lag, na.rm = TRUE), post + overidpost)
-    expect_equal(max(largest_lead, na.rm = TRUE), pre + overidpre)
-
-
-})
-
-test_that("removes the correct column when normalize < 0", {
-
-    post  <- 2
-    pre  <- 3
-    overidpre <- 4
     overidpost <- 7
     normalize <- 15
 
@@ -77,6 +51,29 @@ test_that("throws an error when post + pre + overidpre + overidpost exceeds the 
                           policyvar = "z", idvar = "id", timevar = "t",
                           controls = "x_r", FE = TRUE, TFE = TRUE,
                           post = post, pre = pre, overidpre = overidpre, overidpost = overidpost, normalize = normalize, cluster = TRUE))
+
+})
+
+test_that("removes the correct column when normalize < 0", {
+
+    M  <- 2
+    G  <- 3
+    LG <- 4
+    LM <- 7
+    normalize <- -2
+
+    outputs <- EventStudy(estimator = "OLS", data = df_sample_dynamic, outcomevar = "y_base",
+                          policyvar = "z", idvar = "id", timevar = "t",
+                          controls = "x_r", FE = TRUE, TFE = TRUE,
+                          M = M, G = G, LG = LG, LM = LM, normalize = normalize, cluster = TRUE)
+
+    leads_lags      <- outputs[[1]]$term
+
+    normalization_column <- paste0("z", "_fd_lead", (-1 * normalize))
+
+    expect_equal(stringr::str_extract(normalization_column, "lead"), "lead")
+    expect_true(!normalization_column %in% leads_lags)
+    expect_true(-1 * normalize > 0)
 
 })
 

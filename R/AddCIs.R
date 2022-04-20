b0b9347d82d1ca866596ff200dc044edeb01d97d
Richard Calvo <97416734+rcalvo12@users.noreply.github.com>
2022-04-20 17:26:38 -0400

    #4 Edit AddCI for normalization and policyvar

--- R/AddCIs.R
+++ R/AddCIs.R
@@ -13,21 +13,37 @@
 #'
 
 AddCIs <- function(Estimates, CI = 0.95) {
-    if ((class(Estimates) != "lm_robust") | (typeof(Estimates) != "list")) {
-        stop("Estimates should be a list with coefficient estimates and standard errors")
-    }
+    if ((class(Estimates) != "list") | typeof(Estimates) != "list" |
+         class(Estimates[[1]]) != "lm_robust" | class(Estimates[[2]]) != "list")
+         {stop("Estimates should be a list of length 2 with Estimates[[1]] an 'lm_robust' list estimates
+                and standard errors and Estimates[[2]] a list of arguments. Should be an output from EventStudy().")}
     if (!is.numeric(CI) | CI < 0 | CI > 1) {stop("CI should be a rational number between 0 and 1, inclusive.")}
 
-    df <- tidy(Estimates)
+    df <- tidy(Estimates[[1]])
+
+    policyvar <- Estimates[[2]]$policyvar
+    normalize <- Estimates[[2]]$normalize
+
+    if (normalize < 0) {
+        normalization_column <- paste0(policyvar, "_fd_lead", (-1 * normalize))
+    } else if (normalize == 0){
+        normalization_column <- paste0(policyvar, "_fd")
+    } else {
+        normalization_column <- paste0(policyvar, "_fd_lag", (normalize))
+    }
+
+    terms <- Estimates[[1]]$term[startsWith(Estimates[[1]]$term, paste0(policyvar, "_")) &
+                                 Estimates[[1]]$term != normalization_column]
 
     percentile <- CI + ((1 - CI)/2)
 
+    df_CI <- df %>%
+          filter(term %in% terms) %>%
+          mutate(ci_lower = estimate - std.error * qnorm(percentile),
+                 ci_upper = estimate + std.error * qnorm(percentile)) %>%
+          select(term, ci_lower, ci_upper)
 
-    df <- df %>%
-          mutate(ci_lower = ifelse(grepl("_lead",term, fixed = T), estimate - std.error * qnorm(percentile),
-                                   ifelse(grepl("_lag", term, fixed = T), estimate - std.error * qnorm(percentile), NA)),
-                 ci_upper = ifelse(grepl("_lead",term, fixed = T), estimate + std.error * qnorm(percentile),
-                                   ifelse(grepl("_lag", term, fixed = T), estimate + std.error * qnorm(percentile), NA)),)
+    df <- left_join(df, df_CI, by = "term")
 
     return(df)
 }

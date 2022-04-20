c91ebc9431536b21c09f552d790adea9a3de45a8
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2022-04-20 17:23:36 -0400

    #4 Setup framework for conditional plotting

--- R/EventStudyPlot.R
+++ R/EventStudyPlot.R
@@ -16,21 +16,33 @@
 #'
 #' @examples
 
-EventStudyPlot <- function(data, estimates, CI, Supt, Preeventcoeffs, Posteventcoeffs, Nozeroline, Smpath) {
+EventStudyPlot <- function(data, estimates, CI, Supt = .95, Preeventcoeffs, Posteventcoeffs, Nozeroline = FALSE, Smpath) {
 
     df_plotting <- PreparePlottingData(data)
 
+    plot_Supt <- if(!is.null(Supt)) TRUE else FALSE
+
+    if (plot_Supt) {
+
+        df_Supt <- AddSuptBand(data, 1000, conf_level = Supt)
+        df_plotting <- merge(df_plotting, df_Supt, by = "term")
+    }
+
+
+    p_Nozeroline <- if(Nozeroline) NULL else ggplot2::geom_hline(yintercept = 0, color = "green", linetype = "dashed")
+    p_Supt <- if(plot_Supt) ggplot2::geom_linerange(data = df_plotting, ggplot2::aes(ymin = lower, ymax = upper)) else NULL
+
     ggplot2::ggplot(df_plotting, ggplot2::aes(x = term, y = estimate)) +
-        ggplot2::geom_point() +
+        p_Supt +
+        ggplot2::geom_point(color = "#006600", size = 3) +
         ggplot2::labs(
             x = "Event time",
             y = "Coefficient"
-        ) +
+            ) +
+        p_Nozeroline +
         ggplot2::theme_bw() +
         ggplot2::theme(
             panel.grid = ggplot2::element_blank()
-        )
-
-
+            )
 
 }

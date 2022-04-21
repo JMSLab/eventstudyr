2034bb4277ee7fd94fecdca34e1b68ab8e1f29f2
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2022-04-21 16:23:17 -0400

    #4 Use EventStudy output for main data

--- R/AddSuptBand.R
+++ R/AddSuptBand.R
@@ -13,16 +13,16 @@
 #' @examples
 #'
 
-AddSuptBand <- function(EventStudy_estimate, num_sim, conf_level, seed = 1234) {
+AddSuptBand <- function(estimates, num_sim, conf_level, seed = 1234) {
 
-    if ((class(EventStudy_estimate) != "lm_robust") & (typeof(EventStudy_estimate) != "list")) {
-    stop("EventStudy_estimate is not a data frame with coefficient estimates and standard errors")
+    if ((class(estimates) != "lm_robust") & (typeof(estimates) != "list")) {
+    stop("estimates is not a data frame with coefficient estimates and standard errors")
     }
     if (! is.numeric(num_sim) | num_sim %% 1 != 0) {stop("num_sim should be a whole number.")}
     if (! is.numeric(conf_level) | conf_level < 0 | conf_level > 1) {stop("conf_level should be a rational number between 0 and 1, inclusive.")}
     if (! is.numeric(seed) | seed %%  1 != 0) {stop("seed should be an integer.")}
 
-    vcov_matrix <- EventStudy_estimate$vcov
+    vcov_matrix <- estimates$vcov
     v_std_errors <- t(sqrt(diag(vcov_matrix)))
     draws <- MASS::mvrnorm(n = num_sim, mu = rep(0, nrow(vcov_matrix)), Sigma = vcov_matrix)
     t <- draws / (v_std_errors %x% matrix(rep(1, num_sim)))
@@ -38,9 +38,9 @@ AddSuptBand <- function(EventStudy_estimate, num_sim, conf_level, seed = 1234) {
         critical_value = t[floor(conf_level_num_sim) + 1]
     }
 
-    df <- data.frame("term" = EventStudy_estimate$term,
-               "lower" = (EventStudy_estimate$coefficients) - (critical_value * EventStudy_estimate$std.error),
-               "upper" = (EventStudy_estimate$coefficients) + (critical_value * EventStudy_estimate$std.error)
+    df <- data.frame("term" = estimates$term,
+               "lower" = (estimates$coefficients) - (critical_value * estimates$std.error),
+               "upper" = (estimates$coefficients) + (critical_value * estimates$std.error)
                )
 
     v_terms_to_plot <- stringr::str_detect(df[, "term"], "fd|lag|lead")

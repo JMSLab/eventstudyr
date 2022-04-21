9b80486171545ef495d4d5050df7b46889a564b1
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2022-04-21 16:22:14 -0400

    #4 Use EventStudy output and make x-axis labels

--- R/PreparePlottingData.R
+++ R/PreparePlottingData.R
@@ -1,42 +1,77 @@
 
 
-PreparePlottingData <- function(data) {
+#' Title
+#'
+#' @param df_tidy_estimates
+#' @param policyvar
+#' @param post
+#' @param overidpost
+#' @param pre
+#' @param overidpre
+#' @param normalize
+#'
+#' @return
+#' @import stringr
+#' @export
+#'
+#' @examples
 
-    df_tidy <- estimatr::tidy(data)
+PreparePlottingData <- function(df_tidy_estimates, policyvar, post, overidpost, pre, overidpre, normalize) {
 
-    v_terms_to_plot <- stringr::str_subset(df_tidy[, "term"], "fd|lag|lead")
+    largest_lead <- pre + overidpre
+    largest_lag <- post + overidpost - 1
 
-    df_order <- data.frame(
-        term = v_terms_to_plot,
-        order = stringr::str_extract(v_terms_to_plot, "\\d$"),
-        no_fd = ifelse(stringr::str_detect(v_terms_to_plot, "fd", negate = TRUE), 1, 0),
-        lead_or_lag = stringr::str_extract(v_terms_to_plot, "lead|lag")
-    )
+    if (normalize < 0) {
+        if (normalize == -(largest_lead + 1)) {
+            normalization_column <- paste0(policyvar, "_lead", (-1 * (normalize + 1)))
+        } else {
+            normalization_column <- paste0(policyvar, "_fd_lead", (-1 * normalize))
+        }
+    } else if (normalize == 0){
+        if (normalize == largest_lag + 1) {
+            normalization_column <- paste0(policyvar, "_lag", (normalize))
+        } else {
+            normalization_column <- paste0(policyvar, "_fd")
+        }
+    } else {
+        if (normalize == largest_lag + 1) {
+            normalization_column <- paste0(policyvar, "_lag", (normalize))
+        } else {
+            normalization_column <- paste0(policyvar, "_fd_lag", (normalize))
+        }
+    }
 
-    first_lead_logic <- df_order["no_fd"] == 1 & df_order["lead_or_lag"] == "lead"
-    first_lead <- df_order[first_lead_logic, "term"]
-    t_0_term <- stringr::str_subset(v_terms_to_plot, "fd$")
 
-    last_lag_logic <- df_order["no_fd"] == 1 & df_order["lead_or_lag"] == "lag"
-    last_lag <- df_order[last_lag_logic, "term"]
+    first_lead <- paste0(policyvar, "_lead", largest_lead)
+    first_lead_integer <- stringr::str_extract(first_lead, "[0-9]+$")
+    first_lead_label <- paste0("-", first_lead_integer, "+")
 
-    df_leads <- df_order[df_order["no_fd"] == 0 & df_order["lead_or_lag"] == "lead", ]
-    df_leads <- df_leads[!is.na(df_leads["no_fd"]), ]
-    df_leads <- df_leads[order(df_leads$order, decreasing = TRUE), ]
+    v_leads <- paste0(policyvar, "_fd_lead", largest_lead:1)
+    v_leads_integer <- stringr::str_extract(v_leads, "[0-9]+$")[-1]
+    v_leads_label <- paste0("-", v_leads_integer)
 
-    v_leads <- df_leads$term
+    t_0_term <- paste0(policyvar, "_fd")
+    t_0_term_label <- "0"
 
-    df_lags <- df_order[df_order["no_fd"] == 0 & df_order["lead_or_lag"] == "lag", ]
-    df_lags <- df_lags[!is.na(df_lags["no_fd"]), ]
-    df_lags <- df_lags[order(df_lags$order, decreasing = FALSE), ]
+    v_lags <- paste0(policyvar, "_fd_lag", 1:largest_lag)
+    v_lags_label <- stringr::str_extract(v_lags, "[0-9]+$")
 
-    v_lags <- df_lags$term
+    last_lag <- paste0(policyvar, "_lag", largest_lag + 1)
+    last_lag_integer <- stringr::str_extract(last_lag, "[0-9]+$")
+    last_lag_label <- paste0(last_lag_integer, "+")
 
-    v_terms_to_plot_order <- c(first_lead, v_leads, t_0_term, v_lags, last_lag)
+    v_terms_to_plot_with_normalization <- c(first_lead, v_leads, t_0_term, v_lags, last_lag)
+    v_identify_normalization_term <- v_terms_to_plot_with_normalization != normalization_column
+    v_terms_to_plot_ordered <- v_terms_to_plot_with_normalization[v_identify_normalization_term]
 
-    df_plotting <- df_tidy[df_tidy$term %in% v_terms_to_plot, ]
+    v_terms_to_plot_labels <- c(first_lead_label, v_leads_label, t_0_term_label, v_lags_label, last_lag_label)
 
-    df_plotting["term"] <- factor(df_plotting$term, levels = v_terms_to_plot_order)
+
+    v_all_terms <- df_tidy_estimates[["term"]]
+    v_plotting_terms <- v_all_terms %in% v_terms_to_plot_ordered
+
+    df_plotting <- df_tidy_estimates[v_plotting_terms, ]
+    df_plotting["label"] <- factor(df_plotting$term, levels = v_terms_to_plot_ordered, labels = v_terms_to_plot_labels)
 
     return(df_plotting)
 

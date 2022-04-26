5be555758c816292ad3a777cc0c251bd9223bb7e
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2022-04-26 16:53:07 -0400

    #4 Incorporate ADDCIs

--- R/EventStudyPlot.R
+++ R/EventStudyPlot.R
@@ -16,7 +16,7 @@
 #'
 #' @examples
 
-EventStudyPlot <- function(data, estimates, CI, Supt = .95, Preeventcoeffs, Posteventcoeffs, Nozeroline = FALSE, Smpath) {
+EventStudyPlot <- function(data, estimates, CI = .95, Supt = .95, Preeventcoeffs, Posteventcoeffs, Nozeroline = FALSE, Smpath) {
 
     df_estimates <- estimates[[1]]
     df_estimates_tidy <- estimatr::tidy(estimates[[1]])
@@ -27,23 +27,28 @@ EventStudyPlot <- function(data, estimates, CI, Supt = .95, Preeventcoeffs, Post
     pre <- estimates[[2]]$pre
     overidpre <- estimates[[2]]$overidpre
     normalize <- estimates[[2]]$normalize
+    normalization_column <- estimates[[2]]$normalization_column
 
-    df_plotting <- PreparePlottingData(df_estimates_tidy, policyvar, post, overidpost, pre, overidpre, normalize)
 
     plot_Supt <- if(!is.null(Supt)) TRUE else FALSE
 
     if (plot_Supt) {
 
-        df_Supt <- AddSuptBand(df_estimates, 1000, conf_level = Supt)
-        df_plotting <- merge(df_plotting, df_Supt, by = "term")
+        df_estimates_tidy <- AddSuptBand(df_estimates, 1000, conf_level = Supt)
     }
 
+    df_CI <- AddCIs(df_estimates_tidy, policyvar, normalization_column, CI)
+
+    df_plotting <- PreparePlottingData(df_CI, policyvar, post, overidpost, pre, overidpre, normalization_column)
+
+
     p_Nozeroline <- if(Nozeroline) NULL else ggplot2::geom_hline(yintercept = 0, color = "green", linetype = "dashed")
-    p_Supt <- if(plot_Supt) ggplot2::geom_linerange(data = df_plotting, ggplot2::aes(ymin = lower, ymax = upper)) else NULL
+    p_Supt <- if(plot_Supt) ggplot2::geom_linerange(data = df_plotting, ggplot2::aes(ymin = suptband_lower, ymax = suptband_upper)) else NULL
 
     ggplot2::ggplot(df_plotting, ggplot2::aes(x = label, y = estimate)) +
         p_Nozeroline +
         p_Supt +
+        ggplot2::geom_errorbar(ggplot2::aes(ymin = ci_lower, ymax = ci_upper), width = .2) +
         ggplot2::geom_point(color = "#006600", size = 3) +
         ggplot2::labs(
             x = "Event time",
@@ -53,5 +58,4 @@ EventStudyPlot <- function(data, estimates, CI, Supt = .95, Preeventcoeffs, Post
         ggplot2::theme(
             panel.grid = ggplot2::element_blank()
             )
-
 }

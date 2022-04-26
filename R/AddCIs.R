451c22727736ab1a444ec204dfdc97c80ca6fd6b
Richard Calvo <97416734+rcalvo12@users.noreply.github.com>
2022-04-26 12:48:58 -0400

    #4 Update AddCI for normalization column

--- R/AddCIs.R
+++ R/AddCIs.R
@@ -3,7 +3,7 @@
 #' @param df_estimates, A list containing the outputs of an estimation containing, at least,
 #' coefficient estimates and standard errors.
 #' @param policyvar, A string with the name of the policy variable used in EventStudy()
-#' @param normalize, Specifies the event-time coefficient used to normalize in EventStudy(). Defaults to - pre - 1.
+#' @param normalization_column, A string specifying the name of the column used to normalize in EventStudy().
 #' @param CI, Confidence interval expressed as a rational number between 0 and 1, inclusively. Defaults to 0.95.
 #'
 #'
@@ -11,25 +11,17 @@
 #' @export
 #'
 #' @examples
-#' AddCIs(df_estimates, policyvar = "z", normalize = -3, CI = .95)
+#' AddCIs(df_estimates, policyvar = "z", normalization_column = "z_fd_lead3", CI = .95)
 #'
 #'
 
-AddCIs <- function(df_estimates, policyvar, normalize, CI = 0.95) {
+AddCIs <- function(df_estimates, policyvar, normalization_column, CI = 0.95) {
     if (class(df_estimates) != "data.frame") {stop("df_estimates should be a data frame")}
     if (! "term" %in% colnames(df_estimates) | ! "estimate" %in% colnames(df_estimates) |
         ! "std.error" %in% colnames(df_estimates)) {stop("df_estimates should include columns 'term', 'estimate', and 'std.error'")}
     if (! is.character(policyvar)) {stop("policyvar should be a character.")}
     if (! is.numeric(normalize)) {stop("normalize should be an integer.")}
     if (! is.numeric(CI) | CI < 0 | CI > 1) {stop("CI should be a rational number between 0 and 1, inclusive.")}
-
-    if (normalize < 0) {
-        normalization_column <- paste0(policyvar, "_fd_lead", (-1 * normalize))
-    } else if (normalize == 0){
-        normalization_column <- paste0(policyvar, "_fd")
-    } else {
-        normalization_column <- paste0(policyvar, "_fd_lag", (normalize))
-    }
 
     terms <- df_estimates$term[startsWith(df_estimates$term, paste0(policyvar, "_fd")) |
                                startsWith(df_estimates$term, paste0(policyvar, "_lead")) |

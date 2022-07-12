c8447919c91661d6878d650c255d0ae03b7d40d4
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2022-07-12 11:45:31 -0400

    #4 #10 Remove suptband simulation hard-coding

--- R/AddSuptBand.R
+++ R/AddSuptBand.R
@@ -32,7 +32,7 @@
 #' eventstudy_coefficients = eventstudy_estimates[[2]]$eventstudy_coefficients)
 #'
 
-AddSuptBand <- function(estimates, num_sim, conf_level = .95, seed = 1234, eventstudy_coefficients) {
+AddSuptBand <- function(estimates, num_sim = 1000, conf_level = .95, seed = 1234, eventstudy_coefficients) {
 
     if ((class(estimates) != "lm_robust") & (typeof(estimates) != "list")) {
     stop("estimates is not a data frame with coefficient estimates and standard errors")
@@ -40,6 +40,8 @@ AddSuptBand <- function(estimates, num_sim, conf_level = .95, seed = 1234, event
     if (! is.numeric(num_sim) | num_sim %% 1 != 0) {stop("num_sim should be a natural number.")}
     if (! is.numeric(conf_level) | conf_level < 0 | conf_level > 1) {stop("conf_level should be a real number between 0 and 1, inclusive.")}
     if (! is.numeric(seed) | seed %%  1 != 0) {stop("seed should be an integer.")}
+    if (class(estimates) != "lm_robust" | typeof(estimates) != "list") {stop("estimates should be a lm_robust list.")}
+    if (! is.character(eventstudy_coefficients)) {stop("eventstudy_coefficients should be a character.")}
 
     vcov_matrix_all <- estimates$vcov
     v_terms_to_keep <- colnames(vcov_matrix_all) %in% eventstudy_coefficients

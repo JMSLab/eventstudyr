a9b7275c0802b964e677c911e2fcc6e18da10e9b
Richard Calvo <97416734+rcalvo12@users.noreply.github.com>
2022-10-24 15:49:24 -0400

    #11 Adding SuptBand test for FHS

--- tests/testthat/test-AddSuptBand.R
+++ tests/testthat/test-AddSuptBand.R
@@ -1,3 +1,5 @@
+# OLS -----------------------------------
+
 test_that("check that the correct part of vcov matrix is used", {
     sample_estimation <- EventStudy(estimator = "OLS", data = df_sample_dynamic,
                                     outcomevar = "y_base", policyvar = "z",
@@ -15,4 +17,25 @@ test_that("check that the correct part of vcov matrix is used", {
     expect_true(! "x_r" %in% colnames(vcov_matrix))
 })
 
+# FHS -----------------------------------
+
+test_that("check that the correct part of vcov matrix is used", {
+    data <- df_sample_dynamic[, c("y_base", "z", "id", "t", "x_r", "eta_m")]
+    sample_estimation <- EventStudy(estimator = "FHS", data = data, outcomevar = "y_base", policyvar = "z", idvar = "id",
+                            timevar = "t", controls = "x_r", proxy = "eta_m", FE = TRUE, TFE = TRUE, post = 1,
+                            overidpost = 2, pre = 1, overidpre = 2, normalize = -1, cluster = TRUE)
+
+    eventstudy_coefficients <- sample_estimation[[2]]$eventstudy_coefficients
+    vcov_matrix_all <- sample_estimation[[1]]$vcov
+    v_terms_to_keep <- colnames(vcov_matrix_all) %in% eventstudy_coefficients
+    vcov_matrix     <- vcov_matrix_all[v_terms_to_keep, v_terms_to_keep]
+
+    expect_true(isSymmetric(vcov_matrix, tol = sqrt(.Machine$double.eps)))
+    expect_true(all(eventstudy_coefficients %in% colnames(vcov_matrix)))
+    expect_true(! "x_r" %in% colnames(vcov_matrix))
+})
+
+asymm_index = function(m) {
+    which(m != t(m), arr.ind = TRUE)
+}
 

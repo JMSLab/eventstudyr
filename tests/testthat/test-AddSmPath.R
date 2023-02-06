31593e6fc9559cb52c330251512b903ae233a7b8
Santiago Hermo <santiago_hermo@brown.edu>
2023-02-06 10:13:36 -0500

    Update AddSmPath testing #9 #18

--- tests/testthat/test-AddSmPath.R
+++ tests/testthat/test-AddSmPath.R
@@ -1,6 +1,6 @@
-# Test 
-test_that("test that AddZerosCovar correctly adds zeros where normalized coefficient should go", {
-  
+# Test
+test_that("AddZerosCovar correctly adds zeros where normalized coefficient should go", {
+
   vcov_matrix_all <- diag(rep(3,10))
   eventstudy_coeffs  <- c(paste0("a", 1:2), paste0("a", 4:9))
   rownames(vcov_matrix_all) <- c(eventstudy_coeffs, "a10", "a11")
@@ -9,11 +9,11 @@ test_that("test that AddZerosCovar correctly adds zeros where normalized coeffic
   norm_column  <- "a3"
   coeffs_oder  <- paste0("a", 1:9)
 
-  expected_matrix <- diag(c(rep(3,2), 0, rep(3,5)))
-  rownames(vcov_matrix_all) <- coeffs_oder
-  colnames(vcov_matrix_all) <- coeffs_oder
+  expected_matrix <- diag(c(rep(3,2), 0, rep(3,6)))
+  rownames(expected_matrix) <- paste0("a", 1:9)
+  colnames(expected_matrix) <- paste0("a", 1:9)
 
   covar <- AddZerosCovar(vcov_matrix_all, eventstudy_coeffs, norm_column, coeffs_oder)
-  
+
   expect_equal(covar, expected_matrix)
 })

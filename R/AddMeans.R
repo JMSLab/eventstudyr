331b10670e5042f5dee2944a57ef39dc24a98f40
Melissa Wu <mzwu173@gmail.com>
2023-02-05 21:29:23 -0500

    Revert comments in AddMeans

--- R/AddMeans.R
+++ R/AddMeans.R
@@ -2,20 +2,20 @@
 #'
 #' @param df Data frame containing the additional variables created for estimation.
 #' @param normalization_column Variable whose coefficient will be normalized.
-#' @param policyvar Policy variable.  # Another function uses "A string with the name of the policy variable used in EventStudy()." for this description. It might be good to use this more detailed description and be consistent across functions. - MZW
-#' @param outcomevar Outcome variable.  # Same as above, a more detailed description would be more helpful. - MZW
+#' @param policyvar Policy variable.
+#' @param outcomevar Outcome variable.
 #' @param custom_scalar Scalar to be displayed along y = 0 line if the user doesn't want the mean computed by the package.
 
 
 AddMeans <- function(df, normalization_column, policyvar, outcomevar,
                      custom_scalar = NULL) {
-
+                     
     if (! is.data.frame(df)) {stop("df should be a data frame.")}
     if (! is.character(outcomevar)) {stop("outcomevar should be a character.")}
     if (! is.character(policyvar)) {stop("policyvar should be a character.")}
-    if (! is.character(normalization_column)) {stop("normalization_column should be a character.")}
+    if (! is.character(normalization_column)) {stop("normalization_column should be a character.")}                         
     if (! (is.numeric(custom_scalar) | is.null(custom_scalar))) {stop("custom_scalar should be numeric.")}
-
+                     
     if (is.null(custom_scalar)) {
         if (grepl(paste0(policyvar, "_lead"), normalization_column)) {
             df_change <- df[df[, normalization_column] == 0 & !is.na(df[, normalization_column]), ]
@@ -30,3 +30,4 @@ AddMeans <- function(df, normalization_column, policyvar, outcomevar,
 
     return(mean)
 }
+

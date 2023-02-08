b17155caa5df791e2ff240b215f08118ddb6709e
Santiago Hermo <santiago_hermo@brown.edu>
2023-02-08 14:56:30 -0500

    Add test for multiple norm coefficients in AddZerosCovar #9
    Also ran devtools::test()

--- tests/testthat/test-AddSmPath.R
+++ tests/testthat/test-AddSmPath.R
@@ -17,3 +17,22 @@ test_that("AddZerosCovar correctly adds zeros where normalized coefficient shoul
 
   expect_equal(covar, expected_matrix)
 })
+# Test
+test_that("AddZerosCovar correctly adds zeros where normalized coefficient should be with 2 normalizations", {
+
+    vcov_matrix_all <- diag(rep(3,9))
+    eventstudy_coeffs  <- c(paste0("a", 1:2), paste0("a", 4), paste0("a", 6:10))
+    rownames(vcov_matrix_all) <- c(eventstudy_coeffs, "a11")
+    colnames(vcov_matrix_all) <- c(eventstudy_coeffs, "a11")
+
+    norm_column  <- c("a3", "a5")
+    coeffs_oder  <- paste0("a", 1:10)
+
+    expected_matrix <- diag(c(rep(3,2), 0, 3, 0, rep(3,5)))
+    rownames(expected_matrix) <- paste0("a", 1:10)
+    colnames(expected_matrix) <- paste0("a", 1:10)
+
+    covar <- AddZerosCovar(vcov_matrix_all, eventstudy_coeffs, norm_column, coeffs_oder)
+
+    expect_equal(covar, expected_matrix)
+})

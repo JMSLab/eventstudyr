60e6efb3113b19ca5103339519b13af59e933081
Santiago Hermo <santiago_hermo@brown.edu>
2023-02-11 12:17:43 -0500

    Test for smoothest path #18

--- tests/testthat/test-EventStudyPlot.R
+++ tests/testthat/test-EventStudyPlot.R
@@ -1,5 +1,4 @@
 
-# OLS ---------------------------------------------------------------------
 
 test_that("correctly changes x-axis and y-axis labels", {
 
@@ -185,4 +184,39 @@ test_that("Sup-t bands are wider than confidence intervals", {
 
     expect_equal(num_terms, sum(v_lower_comparison))
     expect_equal(num_terms, sum(v_upper_comparison))
+})
+
+test_that("computed smoothest path for examples is within expectations", {
+
+    estimates <- EventStudy(estimator = "OLS", data = df_sample_dynamic, outcomevar = "y_base",
+                            policyvar = "z", idvar = "id", timevar = "t", controls = "x_r",
+                            post = 3, pre = 2, overidpre = 4, overidpost = 5, normalize = - 3)
+
+    p <- EventStudyPlot(estimates = estimates,
+                        Smpath    = T)
+
+    expect_equal(p$data$smoothest_path, matrix(rep(0, nrow(p$data))))
+
+    estimates <- EventStudy(estimator = "OLS", data = df_sample_dynamic, outcomevar = "y_smooth_m",
+                            policyvar = "z", idvar = "id", timevar = "t", controls = "x_r",
+                            post = 3, pre = 2, overidpre = 4, overidpost = 5, normalize = - 3)
+
+    p <- EventStudyPlot(estimates = estimates,
+                        Smpath    = T)
+
+    normalized_index  <- which(p$data$estimate == 0)
+    normalized_smpath <- p$data$smoothest_path[normalized_index]
+
+    # Expect normalized_smpath to be almost equal to zero
+    expect_true(all(abs(normalized_smpath) < 1e-10))
+
+    max_smpath <- max(p$data$smoothest_path)
+    min_smpath <- min(p$data$smoothest_path)
+
+    max_suptband <- max(p$data$suptband_upper, na.rm = T)
+    min_suptband <- min(p$data$suptband_lower, na.rm = T)
+
+    # Expect smpath to be contained in suptband
+    expect_true(max_smpath < max_suptband)
+    expect_true(min_smpath > min_suptband)
 })

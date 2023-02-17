be970d3325b050c24ae7de7fad7e9c18e2aacad1
Santiago Hermo <santiago_hermo@brown.edu>
2023-02-16 21:23:05 -0500

    Add examples with R #19

--- issue19_smpath_compare/sm_path.R
+++ issue19_smpath_compare/sm_path.R
@@ -8,3 +8,23 @@ setwd("issue19_smpath_compare/")
 write.csv(df_sample_dynamic,
           "data_dynamic.csv", row.names = T)
 
+for (yvar in c("y_base", "y_smooth_m", "y_jump_m")) {
+
+    estimates_ols <- EventStudy(
+        outcomevar = yvar,
+        estimator = "OLS", data = df_sample_dynamic,
+        policyvar = "z", idvar = "id", timevar = "t", controls = "x_r",
+        post = 3, overidpost = 1,
+        pre  = 0, overidpre  = 3,
+        normalize = - 1,
+    )
+    EventStudyPlot(
+        estimates = estimates_ols,
+        Smpath    = TRUE
+    ) -> p
+
+    ggsave(sprintf("R/%s.png", yvar), dpi = 250,
+           width = 7, height = 5)
+}
+
+

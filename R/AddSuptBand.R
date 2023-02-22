74005bdf9df6a7a0db1716495ffacb0f3e97604f
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2023-02-22 11:01:56 -0500

    Remove check on estimate type #17
    We want to make sure that the estimates argument is a data frame.
    However, the list test was not catching variables that are lists but not
    data frames, such as eventstudy_estimates[[2]], so I removed the test.

--- R/AddSuptBand.R
+++ R/AddSuptBand.R
@@ -53,9 +53,8 @@
 
 AddSuptBand <- function(estimates, num_sim = 1000, conf_level = .95, seed = 1234, eventstudy_coefficients) {
 
-    if (! class(estimates) %in% c("lm_robust", "iv_robust") & (typeof(estimates) != "list")) {
+    if (! class(estimates) %in% c("lm_robust", "iv_robust")) {
     stop("estimates is not a data frame with coefficient estimates and standard errors")
-        # correct error message doesn't appear when I enter estimates=eventstudy_estimates[[2]] in the example - MZW
     }
     if (! is.numeric(num_sim) | num_sim %% 1 != 0 | num_sim <= 0) {stop("num_sim should be a natural number.")}
     if (! is.numeric(conf_level) | conf_level < 0 | conf_level > 1) {stop("conf_level should be a real number between 0 and 1, inclusive.")}

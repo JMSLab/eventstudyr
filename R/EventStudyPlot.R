6873989772f099ca472657df07704a68541a9d16
Santiago Hermo <santiago_hermo@brown.edu>
2023-02-27 20:07:30 -0500

    tiny change

--- R/EventStudyPlot.R
+++ R/EventStudyPlot.R
@@ -153,7 +153,7 @@ EventStudyPlot <- function(estimates,
         df_estimates_tidy <- AddCIs(df_estimates_tidy, policyvar, eventstudy_coefficients, conf_level)
     }
 
-# Optionally Test For Pretrends/Levelling-Off -------------------------------
+# Optionally Test For Pretrends/Levelling-Off -----------------------------
 
     df_test_linear <- TestLinear(estimates = estimates, pretrends = Preeventcoeffs, leveling_off = Posteventcoeffs)
 

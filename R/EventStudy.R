a63e4108aa22b23d66bca288af47990a71612a5b
Santiago Hermo <santiago_hermo@brown.edu>
2023-03-11 17:12:04 -0500

    Drop comments in event-study description #17

--- R/EventStudy.R
+++ R/EventStudy.R
@@ -2,11 +2,11 @@
 #'
 #' @param estimator Accepts one of "OLS" or "FHS". If "FHS" is specified, implements IV estimator in Freyaldenhoven et al. 2019.
 #' @param data Data frame that contains the variables of interest.
-#' @param outcomevar Character indicating column of outcome variable y. # Instead of "should be a character" change to "Character for variable indicating ..." for concision? - MZW
+#' @param outcomevar Character indicating column of outcome variable y.
 #' @param policyvar Character indicating column of policy variable z.
 #' @param idvar Character indicating column of units.
 #' @param timevar Character indicating column of time periods.
-#' @param controls Character indicating optional vector of controls q. # What is the q for? - MZW
+#' @param controls Character indicating optional vector of controls q.
 #' @param proxy Character indicating column of variable that is thought to be affected by the confound but not by the policy.
 #' Should be specified if and only if estimator is specified as "FHS".
 #' @param proxyIV Character of column to be used as an instrument. Should be specified if and only if estimator is specified as "FHS".
@@ -18,16 +18,18 @@
 #' @param post Whole number indicating the number of periods in the past before which the past values of the policy
 #' are not supposed to affect the value of the outcome. Corresponds to M in equation (2) of
 #' [Freyaldenhoven et al. (2021)](https://www.nber.org/system/files/working_papers/w29170/w29170.pdf).
-#' @param overidpost Optional whole number indicating the number of event times after "post" to be included in estimation. Defaults to 1.
-#' Corresponds to L_M in equation (2) of [Freyaldenhoven et al. (2021)](https://www.nber.org/system/files/working_papers/w29170/w29170.pdf).  # Delete "should be a whole number" and instead say "Optional whole number of event times..." ? - MZW
+#' @param overidpost Optional whole number indicating the number of event times after "post" to be included in estimation. 
+#' Defaults to 1.
+#' Corresponds to L_M in equation (2) of [Freyaldenhoven et al. (2021)](https://www.nber.org/system/files/working_papers/w29170/w29170.pdf).
 #' @param pre Whole number indicating the number of periods in the future after which the future values of the policy are
 #' not supposed to affect the value of the outcome today. Corresponds to G in equation (2) of
 #' [Freyaldenhoven et al. (2021)](https://www.nber.org/system/files/working_papers/w29170/w29170.pdf).
-#' @param overidpre Optional whole number indicating the number of event times earlier than -"pre" to be included in estimation. Defaults to "post" + "pre".
+#' @param overidpre Optional whole number indicating the number of event times earlier than -"pre" to be included in estimation. 
+#' Defaults to "post" + "pre".
 #' Corresponds to L_G in equation (2) of [Freyaldenhoven et al. (2021)](https://www.nber.org/system/files/working_papers/w29170/w29170.pdf).
 #' @param normalize Specifies the event-time coefficient to be normalized. Defaults to - pre - 1.
 #' @param anticipation_effects_normalization If set to TRUE, runs default process and switches coefficient to be normalized to 0
-#' when there are anticipation effects. If set to FALSE, does not make switch. Defaults to TRUE  # This variable name is very long - MZW
+#' when there are anticipation effects. If set to FALSE, does not make switch. Defaults to TRUE.
 #'
 #' @return A list that contains the estimation output and an object containing the arguments passed to the function
 #' @import dplyr

f5b53ddc7fba15a2d7a2effc1f54d68dc26d9e10
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2023-03-14 15:02:21 -0400

    Add link to EventStudy() documentation #17 #24
    Co-authored-by: Santiago Hermo <45404755+santiagohermo@users.noreply.github.com>

--- R/EventStudyFHS.R
+++ R/EventStudyFHS.R
@@ -1,7 +1,7 @@
 #' Implements proxy IV estimator proposed in [Freyaldenhoven, Hansen, and Shapiro](https://pubs.aeaweb.org/doi/pdfplus/10.1257/aer.20180609)
 #'
 #' @param prepared_model_formula A formula object created in PrepareModelForumla that is passed to EventStudy.
-#' @param prepared_data Data frame containing all of the parameters required for EventStudy() plus leads and
+#' @param prepared_data Data frame containing all of the parameters required for [EventStudy()] plus leads and
 #' lags of the first differenced policy variable and leads and lags of the policy variable.
 #' @param idvar Character indicating column of units.
 #' @param timevar Character indicating column of time periods.

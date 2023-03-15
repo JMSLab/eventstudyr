e0b63cc4361998bc27fdc8020436697274be0f46
jmshapir <jesse.m.shapiro@gmail.com>
2023-03-15 19:47:34 -0400

    Update R/AddCIs.R
    Co-authored-by: Santiago Hermo <45404755+santiagohermo@users.noreply.github.com>

--- R/AddCIs.R
+++ R/AddCIs.R
@@ -43,8 +43,9 @@
 
 AddCIs <- function(df_estimates, eventstudy_coefficients, conf_level = 0.95) {
     if (! inherits(df_estimates, "data.frame")) {stop("df_estimates should be a data frame")}
-    if (! "term" %in% colnames(df_estimates) | ! "estimate" %in% colnames(df_estimates) |
-        ! "std.error" %in% colnames(df_estimates)) {stop("df_estimates should include columns 'term', 'estimate', and 'std.error'")}
+    if (!all(c("term", "estimate", "std.error") %in% colnames(df_estimates))) {
+        stop("df_estimates should include columns 'term', 'estimate', and 'std.error'")
+    }
     if (! is.character(eventstudy_coefficients)) {stop("eventstudy_coefficients should be a character vector.")}
     if (! is.numeric(conf_level) | conf_level < 0 | conf_level > 1) {stop("conf_level should be a real number between 0 and 1, inclusive.")}
 

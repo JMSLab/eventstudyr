a965786ab0ad7ab612e7554cb671b59329c6f909
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2023-03-20 16:13:54 -0400

    Save df_sample as .rda #25

--- examples/source/exampledata/build.R
+++ examples/source/exampledata/build.R
@@ -1,11 +1,10 @@
 library(haven)
 
 indir  <- 'examples/source/raw/eventstudy_illustration_data/orig'
-outdir <- 'data'
 
 df_sample_dynamic <- read_dta(sprintf('%s/simulation_data_dynamic.dta', indir))
-save(df_sample_dynamic, file = sprintf('%s/df_sample_dynamic.RData', outdir))
+usethis::use_data(df_sample_dynamic, overwrite = TRUE, version = 3)
 
 df_sample_static  <- read_dta(sprintf('%s/simulation_data_static.dta', indir))
-save(df_sample_static, file = sprintf('%s/df_sample_static.RData', outdir))
+usethis::use_data(df_sample_static, overwrite = TRUE, version = 3)
 

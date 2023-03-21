e5c8816336d85101add93c3384ba2d55b146a22f
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2023-03-21 11:32:58 -0400

    Create example data from data-raw scripts #25

--- data-raw/df_EventStudyOLS_example.R
+++ data-raw/df_EventStudyOLS_example.R
@@ -40,4 +40,4 @@ df_EventStudyOLS_example <- df_lag_lead[c("id", "t", "z", "y_base", "x_r", "z_fd
                                           "z_fd_lead1", "z_fd_lead2", "z_fd_lead3",
                                           "z_fd_lag1", "z_fd_lag2", "z_lag3","z_lead3")]
 
-usethis::use_data(df_EventStudyOLS_example, overwrite = TRUE, 3)
+usethis::use_data(df_EventStudyOLS_example, overwrite = TRUE, version = 3)
--- data-raw/df_sample_static.R
+++ data-raw/df_sample_static.R
@@ -4,4 +4,4 @@ library(haven)
 
 indir  <- 'examples/source/raw/eventstudy_illustration_data/orig'
 df_sample_static  <- read_dta(sprintf('%s/simulation_data_static.dta', indir))
-usethis::use_data(df_sample_static, overwrite = TRUE, 3)
+usethis::use_data(df_sample_static, overwrite = TRUE, version = 3)

bcda2f03453578ae9666fd2d94d4559118ed4418
Santiago Hermo <santiago_hermo@brown.edu>
2023-05-02 10:28:36 -0400

    First version of ComputeShifts #31

---
+++ R/ComputeShifts.R
@@ -0,0 +1,99 @@
+#' Adds leads or lags of a variable as new columns in a panel dataset
+#'
+#' @description `ComputeShifts` adds rows to a panel dataset with leads or lags of a variable.
+#'
+#' @param df Data frame that will be modified.
+#' @param idvar Character indicating column of optional grouping variable.
+#' @param timevar Character indicating column of time periods.
+#' @param shiftvar Character indicating column of variable that will be shifted.
+#' @param shiftvalues Numeric vector specifying the leads/lags to be computed. For example, '-1:1' will compute the lag, current, and lead values.
+#' @param unbalanced Logical indicating whether the dataset is unbalanced.
+#' @param return_df Logical indicating whether the function should return a data frame.
+#'
+#' @return The passed data frame augmented with columns called lagvar_lagN, where lagvar is the value specified and N is the selected lag(s).
+#'
+#' @seealso \link[data.table]{shift}
+#'
+#' @examples
+#' ComputeShifts(
+#'     example_data,
+#'     idvar = "id",
+#'     timevar = "t",
+#'     shiftvar = "z",
+#'     shiftvalues = -2:2
+#' )
+#'
+#'
+#' @importFrom data.table setDT setorderv shift := CJ
+#' @keywords internal
+#' @noRd
+
+ComputeShifts <- function(df, idvar, timevar, shiftvar, shiftvalues,
+                          unbalanced = FALSE, return_df = TRUE) {
+    if (! is.data.frame(df)) {
+        stop("df should be a data frame.")
+    }
+    if ((! is.character(idvar))) {
+        stop("idvar should be a character.")
+    }
+    if (! is.character(timevar)) {
+        stop("timevar should be a character.")
+    }
+    if (! is.character(shiftvar)) {
+        stop("shiftvar should be a character.")
+    }
+    if (! is.logical(unbalanced)) {
+        stop("unbalanced should be logical.")
+    }
+    if (! is.numeric(shiftvalues)) {
+        stop("shiftvalues should be numeric.")
+    }
+    if (0 %in% shiftvalues & length(shiftvalues) == 1) {
+        stop("shiftvalues must be different than 0 to compute leads/lags.")
+    }
+
+    data.table::setDT(df)
+    data.table::setorderv(df, cols = c(idvar, timevar))
+
+    lags  <-    shiftvalues[shiftvalues >  0]
+    leads <- -1*shiftvalues[shiftvalues <= 0]
+
+    compute_shifts <- function(dat, idvar, shiftvar, shiftvalues, lags, leads) {
+
+        if (any(shiftvalues > 0)) {
+            dat[, paste0(shiftvar, "_lag", lags) :=
+                     data.table::shift(get(shiftvar), lags, type = "lag"),
+                 by = c(idvar)]
+        }
+        if (any(shiftvalues <= 0)) {
+            dat[, paste0(shiftvar, "_lead", leads) :=
+                     data.table::shift(get(shiftvar), leads, type = "lead"),
+                 by = c(idvar)]
+        }
+        return(dat)
+    }
+
+    if (!unbalanced) {
+        df = compute_shifts(df, idvar, shiftvar, shiftvalues, lags, leads)
+    } else {
+        ## Create dataset with all combinations to compute shifts
+        all_combinations <- CJ(unique(df[[idvar]]), unique(df[[timevar]]))
+        setnames(all_combinations, new = c(idvar, timevar))
+
+        vars_to_keep <- c(idvar, timevar, shiftvar)
+        df_all <- merge(df[, ..vars_to_keep], all_combinations,
+                        by = c(idvar, timevar), all = TRUE)
+
+        df_all = compute_shifts(df_all, idvar, shiftvar, shiftvalues, lags, leads)
+
+        ## Bring shifts back to the original dataset
+        df <- merge(df, df_all,
+                    by = c(idvar, timevar), all.x = TRUE)
+    }
+
+    if (return_df) {
+        df <- as.data.frame(df)
+    }
+
+    return(df)
+}

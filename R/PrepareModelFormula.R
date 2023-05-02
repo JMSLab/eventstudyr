79f6cc48d8655500bad30a929412dac98eb7f7cb
Santiago Hermo <santiago_hermo@brown.edu>
2023-05-02 15:00:01 -0400

    Fixes to PrepareModelFormula

--- R/PrepareModelFormula.R
+++ R/PrepareModelFormula.R
@@ -2,12 +2,9 @@
 #' Prepares a formula object for use in [EventStudyOLS()] or [EventStudyFHS()]
 
 #' @param estimator Accepts one of "OLS" or "FHS". If "FHS" is specified, implements IV estimator in Freyaldenhoven et al. 2019.
-#' @param outcomevar Character indicating column of outcome variable y.
-#' @param str_policy_fd Character indicating vector of leads and lags of first differenced policy variable z with the normalization term omitted.
-#' Defaults to omitting the (pre +1)th term.
-#' @param str_policy_lead Character indicating variable of the (pre + overidpre)th lead of the policy variable z.
-#' @param str_policy_lag Character indicating variable of the (post + overidpost)th lag of the policy variable z.
-#' @param controls Character indicating optional vector of controls q.
+#' @param outcomevar Character indicating column of outcome variable.
+#' @param str_policy_vars Character vector indicating event-study variables.
+#' @param controls Character indicating optional vector of controls.
 #' @param proxy Character indicating column of variable that is thought to be affected by the confound but not by the policy.
 #' Should be specified if and only if estimator is specified as "FHS".
 #' @param proxyIV Character of column to be used as an instrument. Should be specified if and only if estimator is specified as "FHS".
@@ -20,22 +17,21 @@
 #' @noRd
 #'
 #' @examples
+#' # For a static model:
 #' PrepareModelFormula(estimator = "OLS", outcomevar = "y_base",
-#'                     str_policy_fd = c("z_fd_lead1", "z_fd_lead2", "z_fd_lead4",
-#'                                       "z_fd_lead5", "z_fd_lead6", "z_fd_lag1",
-#'                                       "z_fd_lag2", "z_fd_lag3", "z_fd_lag4",
-#'                                       "z_fd_lag5", "z_fd_lag6", "z_fd_lag7"),
-#'                     str_policy_lead = "z_lead6",
-#'                     str_policy_lag = "z_lag8",
+#'                     str_policy_vars = "z", static = T)
+#' 
+#' # For a dynamic model:
+#' PrepareModelFormula(estimator = "OLS", outcomevar = "y_base",
+#'                     str_policy_vars = c("z_lead3", "z_fd_lead3", "z_fd_lead2",
+#'                                         "z_fd_lag1", "z_fd_lag2", "z_lag3"),
 #'                     controls = "x_r")
 #'
 #' # If you would like to use IV regression:
 #' PrepareModelFormula(estimator = "FHS",
 #'                     outcomevar = "y_base",
-#'                     str_policy_fd = c("z_fd", "z_fd_lead2", "z_fd_lead3",
-#'                                       "z_fd_lag1", "z_fd_lag2"),
-#'                     str_policy_lead = "z_lead3",
-#'                     str_policy_lag = "z_lag3",
+#'                     str_policy_vars = c("z_lead3", "z_fd_lead3", "z_fd_lead2",
+#'                                         "z_fd", "z_fd_lag1", "z_fd_lag2", "z_lag3"),
 #'                     controls = "x_r",
 #'                     proxy = "eta_m",
 #'                     proxyIV = "z_fd_lead3")
@@ -47,9 +43,7 @@ PrepareModelFormula <- function(estimator, outcomevar,
 
     if (! estimator %in% c("OLS", "FHS"))      {stop("estimator should be either 'OLS' or 'FHS'.")}
     if (! is.character(outcomevar))            {stop("outcomevar should be a character.")}
-    if (! is.character(str_policy_fd))         {stop("str_policy_fd should be a character.")}
-    if (! is.character(str_policy_lead))       {stop("str_policy_lead should be a character.")}
-    if (! is.character(str_policy_lag))        {stop("str_policy_lag should be a character.")}
+    if (! is.character(str_policy_vars))       {stop("str_policy_fd should be a character.")}
     if (! (is.null(controls) | is.character(controls))) {stop("controls should be either NULL or a character.")}
     if (is.null(proxyIV) & estimator == "FHS") {stop("proxyIV must be specified with estimator=FHS")}
 

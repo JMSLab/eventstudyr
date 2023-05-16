e43913a91d0668db61cba6bc8ecd340009466f10
Santiago Hermo <45404755+santiagohermo@users.noreply.github.com>
2023-05-16 16:56:18 -0400

    Update R/ComputeShifts.R
    Co-authored-by: Nathan Schor <70981003+nateschor@users.noreply.github.com>

--- R/ComputeShifts.R
+++ R/ComputeShifts.R
@@ -6,7 +6,7 @@
 #' @param shiftvar Character indicating column of variable that will be shifted.
 #' @param shiftvalues Numeric vector specifying the leads/lags to be computed. For example, '-1:1' will compute the lag, current, and lead values.
 #' @param timevar_holes Logical indicating whether the panel contains gaps in the time variable. Defaults to FALSE.
-#' @param return_df Logical indicating whether the function should return a data frame.
+#' @param return_df Logical indicating whether the function should return a data frame (TRUE) or data.table (FALSE). Defaults to TRUE.
 #'
 #' @return The passed dataset augmented with columns that reflect the desired shift values
 

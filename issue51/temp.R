c4e20e51258c32fb24e36d00b0081290c2f8ace6
zhizhongpu <84325421+zhizhongpu@users.noreply.github.com>
2024-10-29 18:34:16 -0400

    #51 bd MRE for data.table problem

--- issue51/temp.R
+++ issue51/temp.R
@@ -1,35 +1,27 @@
+library(data.table)
 
-test = mtcars |>
+dt = mtcars |>
     as.data.table()
 
 df = mtcars
-normalization_column = 'mpg'
-
-df_change <- df[df[, normalization_column] == 0 & !is.na(df[, normalization_column]), ]
-    df_change <- df[df[, normalization_column] != 0 & !is.na(df[, normalization_column]), ]
-mean <- mean(df_change[[outcomevar]], na.rm = T)
 
 
-if (is.null(custom_scalar)) {
-    if (grepl(paste0(policyvar, "_lead"), normalization_column)) {
-        df_change <- df[df[, normalization_column] == 0 & !is.na(df[, normalization_column]), ]
-    }else {
-        df_change <- df[df[, normalization_column] != 0 & !is.na(df[, normalization_column]), ]
-    }
-    mean <- mean(df_change[[outcomevar]], na.rm = T)
+MockComputeFirstDifferences <- function(data) {
 
-} else {
-    mean <- custom_scalar
+    setDT(data)
+    data[, mpg_fd := 1]
+
 }
 
 
-if (is.null(custom_scalar)) {
-    if (grepl(paste0(policyvar, "_lead"), normalization_column)) {
-        df_change <- df[get(normalization_column) == 0 & !is.na(get(normalization_column))]
-    } else {
-        df_change <- df[get(normalization_column) != 0 & !is.na(get(normalization_column))]
-    }
-    mean <- df_change[, mean(get(outcomevar), na.rm = TRUE)]
-} else {
-    mean <- custom_scalar
+MockEventStudy <- function(data) {
+
+    data <- MockComputeFirstDifferences(data)
+
+    return(0)
 }
+
+MockES(dt)
+names(dt)
+MockES(df)
+names(df)

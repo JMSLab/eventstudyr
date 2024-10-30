a24198519be87ee1800b53e149b4820118099e07
zhizhongpu <84325421+zhizhongpu@users.noreply.github.com>
2024-10-29 23:05:36 -0400

    #51 doc benchmark solutions

--- issue51/temp.R
+++ issue51/temp.R
@@ -18,10 +18,62 @@ MockEventStudy <- function(data) {
 
     data <- MockComputeFirstDifferences(data)
 
-    return(0)
+    return(names(copy))
 }
 
 MockES(dt)
 names(dt)
 MockES(df)
 names(df)
+
+# Potential solution: Shallow copy?
+
+MockEventStudyDeepCopy <- function(data) {
+
+    copy = copy(data)
+    copy <- MockComputeFirstDifferences(copy)
+
+    return(names(copy))
+}
+
+MockEventStudyDeepCopy(dt)
+names(dt)
+
+
+# Time Benchmarks
+dt_large <- rbindlist(replicate(1000000, dt, simplify = FALSE))
+df_large = as.data.frame(dt_large)
+
+# Benchmark against original
+MockEventStudyOrig = function(data) {
+
+    data_dt = as.data.table(data)
+    data_dt <- MockComputeFirstDifferences(data_dt)
+
+    return(names(copy))
+}
+
+MockEventStudyShallowCopy <- function(data) {
+
+    copy = data[T,]
+    copy <- MockComputeFirstDifferences(copy)
+
+    return(names(copy))
+}
+
+time_orig <- system.time(MockEventStudyOrig(df_large))
+print(time_orig) # 1.158
+names(df_large) # no mpg_fd7
+
+time_deep_copy <- system.time(MockEventStudyDeepCopy(dt_large))
+print(time_deep_copy) # 1.147
+names(dt_large) # no mpg_fd7
+
+time_shallow_copy <- system.time(MockEventStudyShallowCopy(dt_large))
+print(time_shallow_copy) # 0.029
+names(dt_large) # no mpg_fd
+# read more on https://github.com/Rdatatable/data.table/issues/3665
+
+time_no_copy <- system.time(MockEventStudy(dt_large))
+print(time_no_copy) # 0.03
+names(dt_large) # mpg_fd

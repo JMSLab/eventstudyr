a6407d793de2708ba4a39706fa010f8840250b82
zhizhongpu <84325421+zhizhongpu@users.noreply.github.com>
2025-03-08 16:27:33 -0500

    #51 bd df->dt in ComputeFirstDifferences

--- R/ComputeFirstDifferences.R
+++ R/ComputeFirstDifferences.R
@@ -1,11 +1,11 @@
 #' Adds first differences of a variable, robustly to gaps in time variable, as new columns in a panel dataset
 #'
-#' @param df Data frame that will be modified.
+#' @param dt Data frame that will be modified.
 #' @param idvar Character indicating column of units.
 #' @param timevar Character indicating column of time periods.
 #' @param diffvar Character indicating column of variable whose first difference will be taken.
 #' @param timevar_holes Logical indicating whether the panel contains gaps in the time variable. Defaults to FALSE.
-#' @param return_df Logical indicating whether the function should return a data frame (TRUE) or data.table (FALSE). Defaults to TRUE.
+#' @param return_df Logical indicating whether the function should return a data frame (TRUE) or data.table (FALSE). Defaults to FALSE
 #'
 #' @return The passed dataset augmented with columns that reflect the desired first differences
 #'
@@ -24,16 +24,16 @@
 #' @keywords internal
 #' @noRd
 
-ComputeFirstDifferences <- function(df, idvar, timevar, diffvar,
-                                    timevar_holes = FALSE, return_df = TRUE) {
-    if (! is.data.frame(df)) {
-        stop("df should be a data frame.")
+ComputeFirstDifferences <- function(dt, idvar, timevar, diffvar,
+                                    timevar_holes = FALSE, return_df = FALSE) {
+    if (! is.data.table(dt)) {
+        stop("dt should be a data table.")
     }
     for (var in c(idvar, timevar, diffvar)) {
         if ((! is.character(var))) {
             stop(paste0(var, " should be a character."))
         }
-        if (! var %in% colnames(df)) {
+        if (! var %in% colnames(dt)) {
             stop(paste0(var, " should be the name of a variable in the dataset."))
         }
     }
@@ -44,41 +44,33 @@ ComputeFirstDifferences <- function(df, idvar, timevar, diffvar,
         stop("return_df should be logical.")
     }
 
-    # ZP: are the above checks necessary? 
-    # At least the first one is already checked by EventStudy() but you might intend that this function be used standalone.?
-
-    data.table::setDT(df) 
-    data.table::setorderv(df, cols = c(idvar, timevar))
-    # ZP: would these above be necessary if we are inputting a dt from EventStudy()?
-    # I think it's necessary only if we intend that this function to be used standalone (outside of EventStudy())
+    data.table::setorderv(dt, cols = c(idvar, timevar))
 
     if (!timevar_holes) {
-        df[, paste0(diffvar, "_fd") := get(diffvar) - shift((get(diffvar))),
+        dt[, paste0(diffvar, "_fd") := get(diffvar) - shift((get(diffvar))),
            by = idvar]
-           # ZP: here we're introducing the problem seen in #51
     } else {
         ## Create dataset with all combinations to compute first differences
-        all_combinations <- data.table::CJ(unique(df[[idvar]]),
-                               min(df[[timevar]]):max(df[[timevar]]))
+        all_combinations <- data.table::CJ(unique(dt[[idvar]]),
+                               min(dt[[timevar]]):max(dt[[timevar]]))
         setnames(all_combinations, new = c(idvar, timevar))
 
-        df_all <- merge(df, all_combinations,
+        dt_all <- data.table::merge.data.table(dt, all_combinations,
                         by = c(idvar, timevar), all = TRUE)
 
-        df_all[, paste0(diffvar, "_fd") := get(diffvar) - shift((get(diffvar))),
+        dt_all[, paste0(diffvar, "_fd") := get(diffvar) - shift((get(diffvar))),
                 by = idvar]
-        # ZP: here we're introducing the problem seen in #51
 
         ## Bring first differences back to the original dataset
         vars_to_keep <- c(idvar, timevar, paste0(diffvar, "_fd"))
 
-        df <- merge(df, df_all[, .SD, .SDcols = vars_to_keep],
+        dt <- data.table::merge.data.table(dt, dt_all[, .SD, .SDcols = vars_to_keep],
                     by = c(idvar, timevar), all.x = TRUE)
     }
 
     if (return_df) {
-        df <- as.data.frame(df)
+        return(data.table::as.data.frame(dt))
     }
 
-    return(df)
+    return(dt)
 }

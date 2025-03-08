9039a9ede4ebb63ba6e2d5c7150af60630f04195
zhizhongpu <84325421+zhizhongpu@users.noreply.github.com>
2025-03-08 17:18:33 -0500

    #51 bd change test-ComputeFirstDifferences.R input to dt

--- tests/testthat/test-ComputeFirstDifferences.R
+++ tests/testthat/test-ComputeFirstDifferences.R
@@ -1,25 +1,27 @@
-test_that("correctly recognizes wrong variable type for grouping variable", {
-    df_test <- read.csv("./input/df_test.csv")
+library(data.table)
 
-    expect_error(ComputeFirstDifferences(df_test, country, "periods", "values"))
+test_that("correctly recognizes wrong variable type for grouping variable", {
+    dt_test <- data.table::fread("./input/df_test.csv")
+
+    expect_error(ComputeFirstDifferences(dt_test, country, "periods", "values"))
 })
 
 test_that("correctly recognizes wrong variable type for diffvar variable", {
-    df_test <- read.csv("./input/df_test.csv")
+    dt_test <- data.table::fread("./input/df_test.csv")
 
-    expect_error(ComputeFirstDifferences(df_test, "country", "periods", values))
+    expect_error(ComputeFirstDifferences(dt_test, "country", "periods", values))
 })
 
 test_that("a column with _fd suffix is added", {
-    df_test <- read.csv("./input/df_test.csv")
+    dt_test <- data.table::fread("./input/df_test.csv")
 
-    df_diff <- ComputeFirstDifferences(df_test, "country", "periods", "values")
+    df_diff <- ComputeFirstDifferences(dt_test, "country", "periods", "values")
 
     expect_true("values_fd" %in% colnames(df_diff))
 })
 
 test_that("correctly computes differences with a balanced dataset", {
-    df <- data.frame(
+    df <- data.table(
         id     = rep(c("A", "B"), each = 4),
         time   = rep(1:4, times = 2),
         policy = c(10, 12, 11, 13, 8, 9, 10, 11),
@@ -32,7 +34,7 @@ test_that("correctly computes differences with a balanced dataset", {
 })
 
 test_that("correctly computes differences with a dataset that has holes in time var", {
-    df <- data.frame(
+    df <- data.table(
         id = c(rep("A", 4), rep("B", 2), rep("C", 3)),
         time = c(1, 2, 4, 5, 2, 3, 2, 3, 4),
         policy = c(10, 12, 13, 14, 8, 9, 10, 11, 12),

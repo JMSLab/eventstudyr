f46c3bb0ea574e88f02d21559fee9b176df8f66a
zhizhongpu <84325421+zhizhongpu@users.noreply.github.com>
2026-01-08 13:13:53 -0500

    #42 fx EventStudyFEOLS_FHS wrong se adjustment

--- R/EventStudyFHS.R
+++ R/EventStudyFHS.R
@@ -131,42 +131,90 @@ EventStudyFHS <- function(prepared_model_formula, prepared_data,
 EventStudyFEOLS_FHS <- function(formula, prepared_data,
                                 idvar, timevar, FE, TFE, cluster) {
 
-    if (cluster) {
-        vcov_fixest <- as.formula(paste0("~", idvar))
-    } else {
-        vcov_fixest <- "iid"
-    }
+    if (! inherits(formula, "formula")) {stop("formula should be a formula")}
+    if (! is.data.frame(prepared_data)) {stop("data should be a data frame.")}
+    if (! is.character(idvar)) {stop("idvar should be a character.")}
+    if (! is.character(timevar)) {stop("timevar should be a character.")}
+    if (! is.logical(FE)) {stop("FE should be either TRUE or FALSE.")}
+    if (! is.logical(TFE)) {stop("TFE should be either TRUE or FALSE.")}
+    if (! is.logical(cluster)) {stop("cluster should be either TRUE or FALSE.")}
+    if (FE & !cluster) {stop("cluster=TRUE required when FE=TRUE.")}
 
-    fhs_output <- fixest::feols(
-        fml = formula,
-        data = prepared_data,
-        vcov = vcov_fixest
-    )
-
-    # Apply the same standard error adjustments as EventStudyFHS
     if (FE & TFE & cluster) {
-        N <- fhs_output$nobs
-        n <- fhs_output$fixef_sizes[1]  # number of clusters (unique id values)
-        # For FE & TFE, K = number of time FEs + number of structural parameters
-        # But we need to be careful - fixest nparams includes the fitted endogenous variable
-        # Let's count the actual event study coefficients (excluding fit_xxx)
-        n_event_coefs <- length(grep("^z_", names(coef(fhs_output)))) - 1  # -1 for fit_eta_m
-        K <- fhs_output$fixef_sizes[2] + n_event_coefs  # time FEs + structural params
 
-        adjustment <- sqrt((N - K) / (N - n - K + 1))
-        fhs_output$se <- fhs_output$se / adjustment
-        fhs_output$cov.scaled <- fhs_output$cov.scaled / (adjustment^2)
+        fhs_output <- fixest::feols(
+            fml = formula,
+            data = prepared_data,
+            vcov = as.formula(paste0("~", idvar))
+        )
+
+        N <- fhs_output$nobs
+        n <- length(unique(prepared_data[[idvar]]))
+        K <- fhs_output$fixef_sizes[[timevar]] + length(coef(fhs_output))
+
+        adjustment_factor <- (N - K) / (N - n - K + 1)
+        fhs_output$se <- fhs_output$se / sqrt(adjustment_factor)
+        fhs_output$cov.scaled <- fhs_output$cov.scaled / adjustment_factor
+
+        # Recalculate statistical inference
+        fhs_output$tstat <- coef(fhs_output) / fhs_output$se
+        fhs_output$pvalue <- 2 * stats::pnorm(abs(fhs_output$tstat), lower.tail = FALSE)
+        fhs_output$conf.low <- coef(fhs_output) - stats::qnorm(0.975) * fhs_output$se
+        fhs_output$conf.high <- coef(fhs_output) + stats::qnorm(0.975) * fhs_output$se
 
     } else if (FE & (!TFE) & cluster) {
-        N <- fhs_output$nobs
-        n <- fhs_output$fixef_sizes[1]  # number of clusters
-        # For FE only, K = 1 (for the FE intercept?) + structural parameters
-        n_event_coefs <- length(grep("^z_", names(coef(fhs_output)))) - 1  # -1 for fit_eta_m
-        K <- 1 + n_event_coefs
 
-        adjustment <- sqrt((N - K) / (N - n - K + 1))
-        fhs_output$se <- fhs_output$se / adjustment
-        fhs_output$cov.scaled <- fhs_output$cov.scaled / (adjustment^2)
+        fhs_output <- fixest::feols(
+            fml = formula,
+            data = prepared_data,
+            vcov = as.formula(paste0("~", idvar))
+        )
+
+        N <- fhs_output$nobs
+        n <- length(unique(prepared_data[[idvar]]))
+        K <- 1 + length(coef(fhs_output))
+
+        adjustment_factor <- (N - K) / (N - n - K + 1)
+        fhs_output$se <- fhs_output$se / sqrt(adjustment_factor)
+        fhs_output$cov.scaled <- fhs_output$cov.scaled / adjustment_factor
+
+        # Recalculate statistical inference
+        fhs_output$tstat <- coef(fhs_output) / fhs_output$se
+        fhs_output$pvalue <- 2 * stats::pnorm(abs(fhs_output$tstat), lower.tail = FALSE)
+        fhs_output$conf.low <- coef(fhs_output) - stats::qnorm(0.975) * fhs_output$se
+        fhs_output$conf.high <- coef(fhs_output) + stats::qnorm(0.975) * fhs_output$se
+
+    } else if ((!FE) & TFE & (!cluster)) {
+
+        fhs_output <- fixest::feols(
+            fml = formula,
+            data = prepared_data,
+            vcov = "iid"
+        )
+
+    } else if ((!FE) & TFE & cluster) {
+
+        fhs_output <- fixest::feols(
+            fml = formula,
+            data = prepared_data,
+            vcov = as.formula(paste0("~", idvar))
+        )
+
+    } else if ((!FE) & (!TFE) & (!cluster)) {
+
+        fhs_output <- fixest::feols(
+            fml = formula,
+            data = prepared_data,
+            vcov = "iid"
+        )
+
+    } else if ((!FE) & (!TFE) & cluster) {
+
+        fhs_output <- fixest::feols(
+            fml = formula,
+            data = prepared_data,
+            vcov = as.formula(paste0("~", idvar))
+        )
     }
 
     return(fhs_output)
